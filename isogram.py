9ad334576f5d81bc4cd53788d3e57bba379d806c
MahendraN <mahendraramalingam8056@gmail.com>
2024-03-31 20:52:14 +0530

    update

---
+++ isogram.py
@@ -0,0 +1,11 @@
+def is_isogram(s):
+    seen = set()
+    for char in s:
+        if char in seen:
+            return False
+        seen.add(char)
+    return True
+
+# Test
+print(is_isogram("hello"))  # Output: False
+print(is_isogram("world"))  # Output: True

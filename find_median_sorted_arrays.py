a36a790234baa48fdaa6ec4da489fd2d1e7923da
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-01 20:47:30 +0530

    update

---
+++ find_median_sorted_arrays.py
@@ -0,0 +1,13 @@
+def find_median_sorted_arrays(nums1, nums2):
+    nums1.extend(nums2)
+    nums1.sort()
+    n = len(nums1)
+    if n % 2 == 0:
+        return (nums1[n // 2 - 1] + nums1[n // 2]) / 2
+    else:
+        return nums1[n // 2]
+        
+n1 = [11,21,34,50]
+n2 = [1,2,3,4,5]
+print("The Median is:", find_median_sorted_arrays(n1,n2))
+

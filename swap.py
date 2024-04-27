1434913c702110f005ac107b2793a588bbf80c16
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-27 19:38:45 +0530

    Update

--- swap.py
+++ swap.py
@@ -1,7 +1,7 @@
 a = 5
 b = 10
-print("The value of a is:", a)
-print("The value of b is:", b)
+print("The Value of a is:", a)
+print("The Value of b is:", b)
 a, b = b, a
 print("The Current value of a is:", a)
 print("The Current  value of b is:", b)

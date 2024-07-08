ac56ef1c747ff00bd6e19aa657bcf80afd6ce4da
MahendraN <mahendraramalingam8056@gmail.com>
2024-07-08 20:17:10 +0530

    Update

--- merge_sorted_lists.py
+++ merge_sorted_lists.py
@@ -2,7 +2,7 @@ def merge_sorted_list(list1, list2):
     value = sorted(list1+list2)
     return value
 
-new = input("Enter the list value separate by commas:")    # Here input get from user for the list values
+new = input("Enter the list Value separate by commas:")    # Here input get from user for the list values
 list1 = [int(x) for x in new.split(',')]                # int(x) used for convert string to numbers
 list2 = [2, 4, 6, 8, 10]
 both = merge_sorted_list(list1, list2)
@@ -11,7 +11,7 @@ print(f"The two lists are {list1} and {list2} then the merging list is:", both)
 # output
 
 """
-    Enter the list value separate by commas:1,3,5,7,9
+    Enter the list Value separate by commas:1,3,5,7,9
     The two lists are [1, 3, 5, 7, 9] and [2, 4, 6, 8, 10] then the merging list is: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 """
